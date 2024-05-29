69a529842049da5bc8b3fdcfa32a5a7acb6bc948
leoBarvenko <leo@barvenko.com>
2024-05-29 14:44:20 +0300

    shop

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -1,16 +1,25 @@
 let clicks = 0;
 let totalClicks = 0;
-let money = 0;  
+let coins = 0; 
+let power = 1; 
+let price = 1;
+let selling = 2;
   let shop = document.getElementsByClassName("shop");
   let notShop = document.getElementsByClassName("notShopCharacter");
+  for(let a = 0; a < shop.length; a++) {
+    shop[a].style.display = "none";
+ }
 function clickPlus() {
-    clicks++;
-    totalClicks++;
+    clicks += power;
+    totalClicks += power;
     displayUpdate();
 }
 
 function displayUpdate() {
-    document.getElementById('masterButton').innerHTML = `total clicks: ${clicks}`;
+    document.getElementById('masterButton').innerHTML = `total clicks: ${totalClicks}`;
+    document.getElementById('clicks').innerHTML = `clicks: ${clicks}`;
+    document.getElementById('coins').innerHTML = `coins: ${coins}`;
+    document.getElementById('upgrade').innerHTML = `${price} coins 🡢 ${selling} clicks`;
 }
 
 function shopOpening() {
@@ -31,4 +40,24 @@ function shopClose() {
          for(let a = 0; a < shop.length; a++) {
             shop[a].style.display = "none";
          }
+}
+function upgrade() {
+if (coins == price) {
+    coins -= price;
+    selling *= 2;
+    price *= 2;
+    power *= 2;
+    displayUpdate();
+} else {
+    alert('you not enogh coins')
+}
+}
+function change() {
+    if (clicks > 10 || clicks == 10) {
+        clicks -= 10;
+        coins++;
+        displayUpdate();
+    } else {
+        alert('you not enogh clicks')
+    }
 }
