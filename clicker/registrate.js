dd17e087a81725742e91d91005640fe1bebfdbf4
leoBarvenko <leo@barvenko.com>
2024-06-06 15:54:40 +0300

    login
    @Rhettwind look pls

---
+++ clicker/registrate.js
@@ -0,0 +1,34 @@
+let data;
+let file;
+let notreg = document.getElementsByClassName("notreg");
+let reg = document.getElementsByClassName("reg");
+for(let i = 0; i < reg.length - 1; i++) {
+    reg[i].style.display = "none"; 
+    notreg[0].style.display = "block";
+    reg[3].style.display = "none";
+}
+function back() {
+    for(let i = 0; i < reg.length - 1; i++) {
+        reg[i].style.display = "none"; 
+        notreg[0].style.display = "block";
+        reg[3].style.display = "none";
+    }    
+}
+function regi() {
+    for(let e = 0; e < reg.length - 1; e++) {
+        notreg[0].style.display = "none"; 
+        reg[e].style.display = "block";
+        reg[3].style.display = "block";
+    }
+        
+    };
+    let password;
+    let nickname;
+    function savelocalstorage() {
+        password = document.getElementById("password").value;
+        nickname = document.getElementById("nickname").value;
+        data = [password, nickname];
+        file = JSON.stringify(data);
+        localStorage.setItem("userdata", file);
+    }
+    
