546f69eb42afbe8889fb3745970e152c8606bea8
leoBarvenko <leo@barvenko.com>
2024-06-19 16:57:44 +0300

    clicker save 0.5
    @Rhettwind

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -2,7 +2,7 @@ let alert = document.getElementById("alert");
 let userDataJSON = localStorage.getItem("userdata");
 let nick = JSON.parse(userDataJSON);
 document.getElementById("nickname").innerHTML = nick[1];
-
+alert.style.display = "none";
 let coins = 0;
 let clicks = 0;
 let totalClicks = 0;
@@ -11,17 +11,20 @@ let price = 1;
 let selling = 2;
 shopClose()
 
-let valuesOfGame = [coins, clicks, totalClicks];
+let valuesOfGame = [coins, clicks, totalClicks, power, price, selling];
 
 try {
     let dataJSON = localStorage.getItem("values");
-        let value = JSON.parse(dataJSON);
-        if (value.length == 3) {
-            coins = value[0];
-            clicks = value[1];
-            totalClicks = value[2];
-            displayUpdate();
-        }
+    let value = JSON.parse(dataJSON);
+    if (value.length == 6) {
+        coins = value[0];
+        clicks = value[1];
+        totalClicks = value[2];
+        power = value[3];
+        price = value[4];
+        selling = value[5];
+        displayUpdate();
+    }
 } catch (error) {
     console.log("you don't have any saved data");
 }
@@ -34,16 +37,17 @@ function clickPlus() {
         alert.style.display = "block";
         setTimeout(function() {
             alert.style.display = "none";
-        }, 10000);
+        }, 5000);
     }
     displayUpdate();
 }
 
 function displayUpdate() {
+    let valuesOfGame = [coins, clicks, totalClicks, power, price, selling];
     document.getElementById('masterButton').innerHTML = `total clicks: ${totalClicks}`;
     document.getElementById('clicks').innerHTML = `clicks: ${clicks}`;
     document.getElementById('coins').innerHTML = `coins: ${coins}`;
-    document.getElementById('upgrade').innerHTML = `${price} coins 🡢 ${selling} clicks`;
+    document.getElementById('upgrade').innerHTML = `${price} coins 🡢 ${selling} power`;
     valuesOfGame = [coins, clicks, totalClicks];
     let dataJSON = JSON.stringify(valuesOfGame);
     localStorage.setItem("values", dataJSON);
